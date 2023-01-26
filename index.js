57f44b24905299f57331f675f2f1e780329e58aa
Nolan Gregory <123116747+Nolobolo28@users.noreply.github.com>
2023-01-26 14:36:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource1 = "images/dice" + randomNumber1 + ".png"; //This will create a new variable structuring the image that should be displayed
+let randomImageSource1 = "images/dice" + randomNumber1 + ".png"; //This will create a new variable structuring the image that should be displayed
 
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1); //This will set the first image's attribute to the random structured src
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
